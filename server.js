8bf7e97c2edcbae56aa8168a9cedf1630c651770
mrizkyridwansyah <mrizkyridwansyah29@gmail.com>
2021-02-02 22:08:39 +0700

    remove cors

--- server.js
+++ server.js
@@ -26,19 +26,38 @@ db.on("error", (err) => console.log(err.message));
 db.on("open", () => console.log("Connected to mongoose"));
 
 //Setup CORS
-var whitelist = ["http://localhost:2100", "http://localhost:3000"];
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
+// var whitelist = ["http://localhost:2100", "http://localhost:3000"];
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 //Setup Authorization
 const { auth, router } = require("./login/routes/auth");
 
