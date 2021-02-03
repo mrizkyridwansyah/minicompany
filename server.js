52d79b3b59fdb7d8fd0bce0c724d18cdb80918a8
mrizkyridwansyah <mrizkyridwansyah29@gmail.com>
2021-02-03 11:34:16 +0700

    cors

--- server.js
+++ server.js
@@ -25,29 +25,19 @@ const db = mongoose.connection;
 db.on("error", (err) => console.log(err.message));
 db.on("open", () => console.log("Connected to mongoose"));
 
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://minicompany.netlify.app"
-  );
+// Setup CORS
+var whitelist = ["https://minicompany.netlify.app"];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
+app.use(cors(corsOptions));
 //Setup Authorization
 const { auth, router } = require("./login/routes/auth");
 
